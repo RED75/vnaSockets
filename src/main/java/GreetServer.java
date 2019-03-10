7860fda69cd22351a6f9c355da20132154057c16
RED <1025591+RED75@users.noreply.github.com>
2019-03-10 23:34:39 +0600

    first code

--- src/main/java/GreetServer.java
+++ src/main/java/GreetServer.java
@@ -26,6 +26,7 @@ public class GreetServer {
                 out.println("unrecognised greeting");
         } catch (IOException e) {
 //            LOG.debug(e.getMessage());
+//            blabla
         }
 
     }
